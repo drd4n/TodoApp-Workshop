570cbd232ad29307ad725173a2fa60ffec509260
Dan Kent <patorn.5@mail.kmutt.ac.th>
2020-12-26 04:52:41 +0700

    {UPDATE} Interactive Hello function to hello view

--- TodoApp/src/main/java/com/doctordan/TodoApp/WebController.java
+++ TodoApp/src/main/java/com/doctordan/TodoApp/WebController.java
@@ -4,14 +4,14 @@
  * and open the template in the editor.
  */
 package com.doctordan.TodoApp;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 /**
  *
  * @author Dan
  */
-@RestController
+@Controller
 public class WebController {
     
     @GetMapping("/hello")
